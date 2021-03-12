5a39e49a2b043c9d3e23cb92aecdb6ff41f51eae
Spadalow <abdullatif.skejic@gmail.com>
2021-03-13 00:03:25 +0100

    TeslA

---
+++ ex12/src/index.js
@@ -0,0 +1,11 @@
+function main () {
+    // Only change code below this line
+var lastName = "Tesla";
+var LastLetterOfLastName = lastName[lastName.length-1];
+    // Only change code above this line
+    return LastLetterOfLastName;
+
+}
+
+console.log(main());
+module.exports = main;
