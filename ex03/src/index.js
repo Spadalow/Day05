c1a8c0e89498d6e8387965fc4c05af244f608da2
Spadalow <abdullatif.skejic@gmail.com>
2021-03-12 21:34:03 +0100

    not sure for backslash

---
+++ ex03/src/index.js
@@ -0,0 +1,9 @@
+function main () {
+    // Only change code below this line
+var myString = "FirstLine\n\t\\SecondLine\nThirdline"
+    // Only change code above this line
+    return myString;
+
+}
+console.log(main());
+module.exports = main;
