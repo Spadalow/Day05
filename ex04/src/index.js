54e0ba34092727dcba2857b778caf5045cb9eaa5
Spadalow <abdullatif.skejic@gmail.com>
2021-03-12 21:42:06 +0100

    S+S

---
+++ ex04/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // Only change code below this line
+var myString = "This is the first string." + "This is the seond string.";
+    // Only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
