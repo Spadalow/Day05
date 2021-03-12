d9088e376faeec196012aec362679c500051a2f2
Spadalow <abdullatif.skejic@gmail.com>
2021-03-12 22:15:17 +0100

    doe

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+var lastNameLenght = "doe".length;
+    // Only change code above this line
+    return lastNameLenght;
+
+}
+console.log (main());
+module.exports = main;
